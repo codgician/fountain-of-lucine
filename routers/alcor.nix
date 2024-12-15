81fa316dc350a0339fa71e7cb081ac4f5c3a61a3
codgician <15964984+codgician@users.noreply.github.com>
2024-12-15 21:21:12 +0800

    alcor: bump to 24.10.0-rc1

--- routers/alcor.nix
+++ routers/alcor.nix
@@ -5,7 +5,7 @@ let
   baremetal = true;
   target = "mediatek";
   variant = "filogic";
-  release = "snapshot";
+  release = "24.10.0-rc1";
   packageLists = builtins.mapAttrs
     (k: v: v { inherit lib release target variant baremetal; })
     bundles;
