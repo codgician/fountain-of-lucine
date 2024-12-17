d3e19ed5e3ddc4e7a485a5fee15c3bd7fe1ba273
codgician <15964984+codgician@users.noreply.github.com>
2024-12-17 23:46:27 +0800

    qiaoying: bump to 24.10.0-rc1

--- routers/qiaoying.nix
+++ routers/qiaoying.nix
@@ -5,7 +5,7 @@ let
   baremetal = true;
   target = "mediatek";
   variant = "filogic";
-  release = "23.05.4";
+  release = "24.10.0-rc1";
   packageLists = builtins.mapAttrs
     (k: v: v { inherit lib release target variant baremetal; })
     bundles;
