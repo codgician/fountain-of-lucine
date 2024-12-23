35636676ce814d517d214542e46ac79457df437e
codgician <15964984+codgician@users.noreply.github.com>
2024-12-24 02:41:55 +0800

    lumidouce: bump to 24.10.0-rc2

--- routers/lumidouce.nix
+++ routers/lumidouce.nix
@@ -5,7 +5,7 @@ let
   baremetal = false;
   target = "x86";
   variant = "64";
-  release = "24.10.0-rc1";
+  release = "24.10.0-rc2";
   packageLists = builtins.mapAttrs
     (k: v: v { inherit lib release target variant baremetal; })
     bundles;
