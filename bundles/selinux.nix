3f22bc0d795e5e6b5660da9ac98087862ee706bc
codgician <15964984+codgician@users.noreply.github.com>
2025-03-19 23:30:33 +0800

    selinux: init

---
+++ bundles/selinux.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+[
+  "-busybox"
+  "busybox-selinux"
+
+  "-procd"
+  "procd-selinux"
+
+  "selinux-policy"
+  "policycoreutils"
+  "policycoreutils-fixfiles"
+  "policycoreutils-load_policy"
+  "policycoreutils-semodule"
+  "policycoreutils-sestatus"
+]
