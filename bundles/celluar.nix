627fdbabaeb202622190cb2a4bf47463f49200d2
codgician <15964984+codgician@users.noreply.github.com>
2025-10-17 20:49:39 +0800

    cellular: add mhi wwan kernel modules

--- bundles/celluar.nix
+++ bundles/celluar.nix
@@ -10,11 +10,19 @@
   "comgt-ncm"
   "qmi-utils"
 
+  "kmod-mhi-net"
+  "kmod-mhi-wwan-ctrl"
+  "kmod-mhi-wwan-mbim"
+  "kmod-wwan"
+
+  "kmod-usb3"
   "kmod-usb-net-cdc-mbim"
   "kmod-usb-net-cdc-ether"
   "kmod-usb-net-cdc-ncm"
   "kmod-usb-net-huawei-cdc-ncm"
   "kmod-usb-net-qmi-wwan"
+  "kmod-usb-net-qmi-wwan-quectel"
   "kmod-usb-serial-option"
   "kmod-usb-serial-qualcomm"
+  "kmod-usb-serial-wwan"
 ]
