f0f215d18a889867b0a7892373ee8fb54f61abca
codgician <15964984+codgician@users.noreply.github.com>
2026-03-30 21:59:07 +0800

    qos: update apps

--- bundles/qos.nix
+++ bundles/qos.nix
@@ -1,10 +1,12 @@
 { lib, release, ... }:
 
 [
+  "luci-app-appfilter"
+  "luci-i18n-appfilter-zh-cn"
   "luci-app-arpbind"
   "luci-i18n-arpbind-zh-cn"
-  "luci-app-eqos"
-  "luci-i18n-eqos-zh-cn"
+  "luci-app-qos"
+  "luci-i18n-qos-zh-cn"
   "luci-app-sqm"
   "luci-i18n-sqm-zh-cn"
 ]
