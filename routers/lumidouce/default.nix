fe872951d32e2c5edc1b0c8642b3ccb28f57922d
codgician <15964984+codgician@users.noreply.github.com>
2026-04-18 00:59:02 +0800

    routers: bump to 25.12.0-rc2

--- routers/lumidouce/default.nix
+++ routers/lumidouce/default.nix
@@ -12,7 +12,7 @@ let
   baremetal = false;
   target = "x86";
   variant = "64";
-  release = "25.12.0-rc1";
+  release = "25.12.0-rc2";
   packageLists = builtins.mapAttrs (
     k: v:
     v {
--- routers/qiaoying/default.nix
+++ routers/qiaoying/default.nix
@@ -12,7 +12,7 @@ let
   baremetal = true;
   target = "mediatek";
   variant = "filogic";
-  release = "25.12.0-rc1";
+  release = "25.12.0-rc2";
   packageLists = builtins.mapAttrs (
     k: v:
     v {
